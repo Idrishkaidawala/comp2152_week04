17424c37ea3d9baa6bcf5b80270b4199c2b77b21
Idrishkaidawala <kaidawalaidrish9@gmail.com>
2025-01-28 11:35:06 -0500

    Lab04-Q4

--- lab04.py
+++ lab04.py
@@ -81,6 +81,8 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+#lab04 - Q4
+belt = []
 
 #lab04 - 01
 #Define the monster power
